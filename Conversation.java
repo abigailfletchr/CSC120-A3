8c19b56ece07a7ac37fb9a844d812b791f7b8ba5
Your Name <you@example.com>
2023-02-14 11:17:40 -0500

    little changes

--- Conversation.java
+++ Conversation.java
@@ -4,34 +4,40 @@ class Conversation {
   public static void main(String[] arguments) {
 
     // a list of our canned phrases
+    // jordan mentioned something about making this static?
     String[] defaultResponse = {"cool!", "nice.", "okie dokie.", "that's interesting."};
 
+    // im gonna be putting the whole convo into this list that is printed as the transcript
     String[] transcript = {};
 
+    // asks the amount of rounds and sets it as an input
     Scanner roundInput = new Scanner(System.in);
     System.out.println("how many rounds? ");
     int userRounds = roundInput.nextInt();
 
+    // basically starts the convo asks how the day is
     Scanner input = new Scanner(System.in);
     System.out.println("how's your day today, anything on your mind? ");
     String howDay = input.nextLine();
 
 
+    // for loop set to the amount of rounds, and, replaces the letters
+    // we still need to check that it's its own word so still need to do that
     for(int i = 0; i < userRounds; i++){
-      if (howDay.contains("I") || howDay.contains("i")){
+      if (howDay.equals("I") || howDay.equals("i")){
         System.out.println(howDay.replace("I", "You"));
-      } else if (howDay.contains("me")) {
+      } else if (howDay.equals("me")) {
         System.out.println(howDay.replace("me", "you"));
-      }else if (howDay.contains("am")) {
+      }else if (howDay.equals("am")) {
         System.out.println(howDay.replace("am", "are"));
-      } else if (howDay.contains("your")) {
+      } else if (howDay.equals("your")) {
       System.out.println(howDay.replace("your", "my"));
-      } else if (howDay.contains("you")) {
+      } else if (howDay.equals("you")) {
         System.out.println(howDay.replace("you", "I"));
-      }else if (howDay.contains("my")) {
+      }else if (howDay.equals("my")) {
         System.out.println(howDay.replace("my", "your"));
       }
-      else if (howDay.contains("are"))
+      else if (howDay.equals("are"))
         System.out.println(howDay.replace("are", "am"));
       else{
         int rand = (int) (Math.random()*(defaultResponse.length+1)) - 1; // maybe -1
@@ -53,3 +59,39 @@ class Conversation {
 
 
 }
+
+
+// .equals() method
+// if (howDay.equals("I") || howDay.equals("i")){
+//   System.out.println(howDay.replace("I", "You"));
+// } else if (howDay.equals("me")) {
+//   System.out.println(howDay.replace("me", "you"));
+// }else if (howDay.equals("am")) {
+//   System.out.println(howDay.replace("am", "are"));
+// } else if (howDay.equals("your")) {
+// System.out.println(howDay.replace("your", "my"));
+// } else if (howDay.equals("you")) {
+//   System.out.println(howDay.replace("you", "I"));
+// }else if (howDay.equals("my")) {
+//   System.out.println(howDay.replace("my", "your"));
+// }
+// else if (howDay.equals("are"))
+//   System.out.println(howDay.replace("are", "am"));
+
+
+  // if (howDay.contains("I") || howDay.contains("i")){
+  //   System.out.println(howDay.replace("I", "You"));
+  // } else if (howDay.contains("me")) {
+  //   System.out.println(howDay.replace("me", "you"));
+  // }else if (howDay.contains("am")) {
+  //   System.out.println(howDay.replace("am", "are"));
+  // } else if (howDay.contains("your")) {
+  // System.out.println(howDay.replace("your", "my"));
+  // } else if (howDay.contains("you")) {
+  //   System.out.println(howDay.replace("you", "I"));
+  // }else if (howDay.contains("my")) {
+  //   System.out.println(howDay.replace("my", "your"));
+  // }
+  // else if (howDay.contains("are"))
+  //   System.out.println(howDay.replace("are", "am"));
+
