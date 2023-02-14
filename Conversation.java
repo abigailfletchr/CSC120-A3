92a3844f020e4e0e8871f6e833f16c4399a32c1f
Your Name <you@example.com>
2023-02-14 00:19:48 -0500

    progress changes

--- Conversation.java
+++ Conversation.java
@@ -3,54 +3,50 @@ import java.util.Scanner;
 class Conversation {
   public static void main(String[] arguments) {
 
-    Scanner userInput = new Scanner(System.in);
+    // a list of our canned phrases
+    String[] defaultResponse = {"cool!", "nice.", "okie dokie.", "that's interesting."};
+
+    String[] transcript = {};
+
+    Scanner roundInput = new Scanner(System.in);
     System.out.println("how many rounds? ");
-    Integer userRounds = userInput.nextInt();
+    int userRounds = roundInput.nextInt();
 
-
-    Scanner userInput1 = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     System.out.println("how's your day today, anything on your mind? ");
-    String howDay = userInput1.nextLine();
+    String howDay = input.nextLine();
 
-    if (howDay.contains("I")){
-      System.out.println(howDay.replace("I", "You"));
-    } else if (howDay.contains("me")) {
-      System.out.println(howDay.replace("me", "you"));
-    }else if (howDay.contains("am")) {
-      System.out.println(howDay.replace("am", "are"));
-    }else if (howDay.contains("you")) {
-      System.out.println(howDay.replace("you", "I"));
-    }else if (howDay.contains("my")) {
-      System.out.println(howDay.replace("my", "your"));
-    }
-    else if (howDay.contains("your"))
+
+    for(int i = 0; i < userRounds; i++){
+      if (howDay.contains("I") || howDay.contains("i")){
+        System.out.println(howDay.replace("I", "You"));
+      } else if (howDay.contains("me")) {
+        System.out.println(howDay.replace("me", "you"));
+      }else if (howDay.contains("am")) {
+        System.out.println(howDay.replace("am", "are"));
+      } else if (howDay.contains("your")) {
       System.out.println(howDay.replace("your", "my"));
-    else if (howDay.contains("are"))
-      System.out.println(howDay.replace("are", "am"));
+      } else if (howDay.contains("you")) {
+        System.out.println(howDay.replace("you", "I"));
+      }else if (howDay.contains("my")) {
+        System.out.println(howDay.replace("my", "your"));
+      }
+      else if (howDay.contains("are"))
+        System.out.println(howDay.replace("are", "am"));
+      else{
+        int rand = (int) (Math.random()*(defaultResponse.length+1)) - 1; // maybe -1
+        System.out.print(defaultResponse[rand]);
+      }
 
-
-
-    for (int i = 1; i  <= userRounds; i++) {
-
-      String sure = ("sure");
-      String wow = ("wow");
-      String great = ("great");
-      String cool = ("coolllll");
-
-      System.out.println("Mhmmm");
-
-  
+      howDay = input.nextLine();
     }
 
+    System.out.println("goodbye!");
 
+    System.out.println("\ntranscript:\n" + transcript);
 
-
-
-
-
-
-
-
+    input.close();
+    roundInput.close();
 
   }
 
