5c73b4bd2d387165955448cb0449b759b934b60e
Your Name <you@example.com>
2023-02-15 23:58:51 -0500

    all code done

--- Conversation.java
+++ Conversation.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Conversation {
   public static void main(String[] arguments) {
 
-    // a list of our canned phrases
-    // jordan mentioned something about making this static?
-    String[] defaultResponse = {"cool!\n", "nice.\n", "okie dokie.\n", "that's interesting.\n"};
+  
+    String[] defaultResponse = {"cool!", "nice.", "okie dokie.", "that's interesting."};
 
-    // im gonna be putting the whole convo into this list that is printed as the transcript
-    String[] transcript = {};
+    ArrayList<String> transcript = new ArrayList<String>();
 
     // asks the amount of rounds and sets it as an input
     Scanner roundInput = new Scanner(System.in);
@@ -17,39 +17,64 @@ class Conversation {
 
     // basically starts the convo asks how the day is
     Scanner input = new Scanner(System.in);
-    System.out.println("how's your day today, anything on your mind? ");
+    String compLine = "how's your day today, anything on your mind?";
+    System.out.println(compLine);
     String howDay = input.nextLine();
-
-
+    // String[] howDayList = howDay.split(" ");
     // for loop set to the amount of rounds, and, replaces the letters
     // we still need to check that it's its own word so still need to do that
     for(int i = 0; i < userRounds; i++){
-      if (howDay.equals("I") || howDay.equals("i")){
-        System.out.println(howDay.replace("I", "You"));
-      } else if (howDay.equals("me")) {
-        System.out.println(howDay.replace("me", "you"));
-      }else if (howDay.equals("am")) {
-        System.out.println(howDay.replace("am", "are"));
-      } else if (howDay.equals("your")) {
-      System.out.println(howDay.replace("your", "my"));
-      } else if (howDay.equals("you")) {
-        System.out.println(howDay.replace("you", "I"));
-      }else if (howDay.equals("my")) {
-        System.out.println(howDay.replace("my", "your"));
-      }
-      else if (howDay.equals("are"))
-        System.out.println(howDay.replace("are", "am"));
-      else{
-        int rand = (int) (Math.random()*(defaultResponse.length+1) -1); // maybe -1
-        System.out.print(defaultResponse[rand]);
-      }
 
+      String[] howDayList = howDay.split(" ");
+      // add to transcript
+      transcript.add(compLine);
+      transcript.add(howDay);
+
+      // * I -> you
+      // * me -> you
+      // * am -> are
+      // * you -> I
+      // * my -> your
+      // * your -> my
+      
+      // see if there is a pronoun to respond to
+      // if(checkForPronouns(howDay)){
+
+        for(int n = 0; n < howDayList.length-1; n++){
+          if(howDayList[n].equals("i")){
+            howDayList[n] = "you";
+          }else if(howDayList[n].equals("I")){
+            howDayList[n] = "you";
+          }else if(howDayList[n].equals("me")){
+            howDayList[n] = "you";
+          }else if(howDayList[n].equals("am")){
+            howDayList[n] = "are";
+          }else if(howDayList[n].equals("you")){
+            howDayList[n] = "I";
+          }else if(howDayList[n].equals("my")){
+            howDayList[n] = "your";
+          }else if(howDayList[n].equals("your")){
+            howDayList[n] = "my";
+          }
+        }
+
+      compLine = String.join(" ", howDayList);
+
+      System.out.println(compLine);
+
+      // get their new input
       howDay = input.nextLine();
     }
 
+    transcript.add(compLine);
+    transcript.add(howDay);
     System.out.println("goodbye!");
+    transcript.add("goodbye!");
 
-    System.out.println("\ntranscript:\n" + transcript);
+    System.out.println("\ntranscript:");
+    for(String m: transcript){
+      System.out.println(m);
+    }
 
     input.close();
     roundInput.close();
@@ -59,39 +84,3 @@ class Conversation {
 
 
 }
-
-
-// .equals() method
-// if (howDay.equals("I") || howDay.equals("i")){
-//   System.out.println(howDay.replace("I", "You"));
-// } else if (howDay.equals("me")) {
-//   System.out.println(howDay.replace("me", "you"));
-// }else if (howDay.equals("am")) {
-//   System.out.println(howDay.replace("am", "are"));
-// } else if (howDay.equals("your")) {
-// System.out.println(howDay.replace("your", "my"));
-// } else if (howDay.equals("you")) {
-//   System.out.println(howDay.replace("you", "I"));
-// }else if (howDay.equals("my")) {
-//   System.out.println(howDay.replace("my", "your"));
-// }
-// else if (howDay.equals("are"))
-//   System.out.println(howDay.replace("are", "am"));
-
-
-  // if (howDay.contains("I") || howDay.contains("i")){
-  //   System.out.println(howDay.replace("I", "You"));
-  // } else if (howDay.contains("me")) {
-  //   System.out.println(howDay.replace("me", "you"));
-  // }else if (howDay.contains("am")) {
-  //   System.out.println(howDay.replace("am", "are"));
-  // } else if (howDay.contains("your")) {
-  // System.out.println(howDay.replace("your", "my"));
-  // } else if (howDay.contains("you")) {
-  //   System.out.println(howDay.replace("you", "I"));
-  // }else if (howDay.contains("my")) {
-  //   System.out.println(howDay.replace("my", "your"));
-  // }
-  // else if (howDay.contains("are"))
-  //   System.out.println(howDay.replace("are", "am"));
-
